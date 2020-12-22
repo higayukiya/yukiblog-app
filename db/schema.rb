4a32ff502aa6846178dcf42861a194bd277b7fdc
higayukiya <higa.yukiya@gmail.com>
2020-12-22 16:46:18 +0900

    変更点記事作成しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_091450) do
+ActiveRecord::Schema.define(version: 2020_12_21_235846) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "contens"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "articles" because of following StandardError
+#   Unknown type 'content' for column 'contens'
 
 end
