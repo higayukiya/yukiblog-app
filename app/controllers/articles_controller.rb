d973ccc4716705b160dbbe7be01f2ea6f0879ad7
higayukiya <higa.yukiya@gmail.com>
2020-12-29 17:40:20 +0900

    show.html.erbのimage2の位置が変わらない。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,19 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only:[:show, :edit, :update]
+
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
-        @article = Article.new
+      @article = Article.new
     end
 
     def create
-        @article = Article.new(article_params)
+      @article = Article.new(article_params)
         if @article.save
             redirect_to article_path(@article), notice: '保存できたよ'
         else
@@ -22,27 +23,29 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(article_params)
+      if @article.update(article_params)
             redirect_to article_path(@article), notice: '更新できました'
-        else
+      else
           flash.now[:error] = '更新できませんでした'
           render :edit
-        end
+      end
     end
 
     def destroy
-        @article = Article.find(params[:id])
-        @article.destroy!
-        redirect_to root_path, notice: '削除に成功しました'
+      @article = Article.find(params[:id])
+      @article.destroy!
+      redirect_to root_path, notice: '削除に成功しました'
     end
 
     private
     def article_params
-        params.require(:article).permit(:title, :contens)
+      params.require(:article).permit(:title, :contens)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
 end
