fd4beba96b85504da659826d5061e1ad1827098b
higayukiya <higa.yukiya@gmail.com>
2021-01-03 11:52:46 +0900

    rollbackとseed修正

--- db/migrate/20201221091450_create_articles.rb
+++ db/migrate/20201221091450_create_articles.rb
@@ -1,8 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :contens
+      t.references :user, null: false
+      t.string :title, null: false
+      t.text :contens, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_005906) do
+ActiveRecord::Schema.define(version: 2020_12_21_235846) do
 
-# Could not dump table "articles" because of following StandardError
-#   Unknown type 'content' for column 'contens'
+  create_table "articles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "contens", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
