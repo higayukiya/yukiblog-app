7e8c720bfc518a783c36ca9b8676f5c60c21995d
higayukiya <higa.yukiya@gmail.com>
2021-01-09 11:35:25 +0900

    likesテーブル作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -25,6 +25,7 @@ class Article < ApplicationRecord
     validate :validate_title_contens_length
 
     has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
     belongs_to :user
 
     def display_created_at
